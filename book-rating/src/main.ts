761a3baaae579b5755069900852068d188fc79bd
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-03-08 12:10:15 +0100

    import/export

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Test } from './test';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
@@ -8,12 +9,6 @@ bootstrapApplication(AppComponent, appConfig)
 
 
 
-class Test {
-
-  constructor(public nachricht: string, public antwort: number) {
-  }
-}
-
 const test = new Test('Hallo TypeScript 😻!!', 42);
 console.log(test.nachricht);
-console.log(test.antwort); // soll funktionieren
+console.log(test.antwort);
