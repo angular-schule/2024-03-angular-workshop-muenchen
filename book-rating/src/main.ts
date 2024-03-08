de0f405ba737f912b0329ddd05fcfe71360720cd
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-03-08 11:48:52 +0100

    TODO!

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -8,8 +8,16 @@ bootstrapApplication(AppComponent, appConfig)
 
 
 
-function test(blubb: string) {
+class Test {
 
+  public nachricht: string;
+
+  constructor(nachricht: string) {
+    this.nachricht = nachricht;
+    // console.log(this.nachricht);
+  }
 }
 
-test('asdasd')
+const test = new Test('Hallo TypeScript 😻!!', 42);
+console.log(test.nachricht);
+console.log(test.antwort); // soll funktionieren
