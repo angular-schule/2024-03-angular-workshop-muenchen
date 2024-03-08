0fca5b77ef54951bcaf634944dd183bfd2263e15
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-03-08 11:01:44 +0100

    git commit

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -4,3 +4,12 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+
+
+function test(blubb: string) {
+
+}
+
+test('asdasd')
