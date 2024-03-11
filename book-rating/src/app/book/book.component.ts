219c6719e336bcaa01a5c7b063ae6290690dbd29
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-03-11 13:21:53 +0100

    Control Flow

--- book-rating/src/app/book/book.component.ts
+++ book-rating/src/app/book/book.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Book } from '../book';
 
 @Component({
   selector: 'app-book',
@@ -9,4 +10,6 @@ import { Component } from '@angular/core';
 })
 export class BookComponent {
 
+  @Input()
+  book: Book | undefined;
 }
