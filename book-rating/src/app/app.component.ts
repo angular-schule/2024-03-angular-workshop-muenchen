252c62b33b32ae10f7518f46e959739dfadd3d46
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-03-11 09:45:52 +0100

    dashboard component

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
-  // kleine Hausaufgabe
-  // ein Buch hat: isbn, title, description, price - Zeige ein Buch an!
-
   title = 'Book Rating 🅰️';
-  blubb = 'Hallo'
+
 }
