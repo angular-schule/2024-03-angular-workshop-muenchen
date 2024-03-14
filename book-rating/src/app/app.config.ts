028e60281dd4db476c2e3bdf926b2b4599b958a1
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-03-14 13:11:26 +0100

    withDebugTracing()

--- book-rating/src/app/app.config.ts
+++ book-rating/src/app/app.config.ts
@@ -6,7 +6,9 @@ import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes,
+      // withDebugTracing()
+    ),
     provideHttpClient()
   ]
 };
