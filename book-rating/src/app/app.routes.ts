d33e364158c3d6b334bdc36f5ee8d358c4743ea1
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-03-14 11:27:51 +0100

    simple routing

--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './books/dashboard/dashboard.component';
+import { BookDetailsComponent } from './books/book-details/book-details.component';
+import { NotFoundComponent } from './books/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'books' },
+  { path: 'books', component: DashboardComponent },
+  { path: 'books/:isbn', component: BookDetailsComponent },
+  { path: '**', component: NotFoundComponent }
+];
