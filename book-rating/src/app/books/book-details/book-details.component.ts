89b798c1d7e5e40548e0e237d802a87903fc05eb
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-03-14 14:17:05 +0100

    concatMap (gut, aber langsam)

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { map, mergeMap } from 'rxjs';
+import { concatMap, map, mergeMap } from 'rxjs';
 import { BookStoreService } from '../shared/book-store.service';
 import { Book } from '../shared/book';
 import { JsonPipe } from '@angular/common';
@@ -24,7 +24,7 @@ export class BookDetailsComponent {
 
     router.paramMap.pipe(
       map(paramMap => paramMap.get('isbn') || ''),
-      mergeMap(isbn => store.getSingle(isbn))
+      concatMap(isbn => store.getSingle(isbn))
     ).subscribe(book => this.book = book);
   }
 }
