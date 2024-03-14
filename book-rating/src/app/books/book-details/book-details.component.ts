249851686941cf56a081ea8ed2d6b15af18d21b6
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-03-14 13:54:27 +0100

    rxjs anti-pattern: verschachtelte subscibes

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { map } from 'rxjs';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss'
 })
 export class BookDetailsComponent {
 
+
+  constructor(router: ActivatedRoute, private store: BookStoreService) {
+
+    router.paramMap.pipe(
+      map(paramMap => paramMap.get('isbn') || ''),
+      map(isbn => store.getSingle(isbn))
+    ).subscribe(x => console.log(x.subscribe(y => console.log(y))));
+  }
 }
